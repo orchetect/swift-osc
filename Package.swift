719d075c9c6a78502619d5a70fbd48d87f610ad6
Steffan Andrews <stef@orchetect.com>
2026-04-30 15:46:46 -0700

    Fixed package name in package manifest

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftOSC",
+    name: "swift-osc",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "SwiftOSC", targets: ["SwiftOSC"])
